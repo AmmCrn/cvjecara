b799832e2ac1440f696ccd0bc05a186486018a51
Ammar Crnkić <ammarcrnkic@gmail.com>
2022-04-16 12:18:32 +0200

    Styling Update

--- views/login.php
+++ views/login.php
@@ -1,18 +1,18 @@
-<div style="width: fit-content; margin: 2em auto; background: rgb(20,0,0); border-radius: 8px; padding: 0 1em">
+<div style="width: fit-content; margin: 2em auto; background: rgb(10,0,0); border-radius: 8px; padding: 0 1em">
     <?php foreach($errors as $err){
         echo "<h4 style='color: red;'>" . $err . "</h4>";
     } ?>
 
-    <form method="post" style="display: block; padding: 1.5em 0 2em">
+    <form method="post" style="display: block; padding: 1.5em 1em 2em">
         <div class="form-elem">
             <label>Username</label>
-            <input type="text" name="user">
+            <input type="text" placeholder="Username..." name="user">
         </div>
         <div class="form-elem">
             <label>Password</label>
-            <input type="password" name="pass">
+            <input type="password" placeholder="Password..." name="pass">
         </div>
         
-        <button class="button button-dark" type="submit">Login</button>
+        <button class="button" style="width: 100%" type="submit">Login</button>
     </form>
 </div>
